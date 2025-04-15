77dd103127b400495d53adb488d23e727624afc4
Michael Donahue <81647737+mdonahue-godaddy@users.noreply.github.com>
2025-04-15 17:02:33 -0500

    add loggin to file

--- scan.sh
+++ scan.sh
@@ -6,6 +6,8 @@ if [[ "${1}" == "clean" ]]; then
 	rm -fv ~/.aws/boto/cache/*.json
 fi
 
+SAVE_DIR=$(pwd)
+
 cd scan_for_sc_parent_stack
 
 python -m venv venv
@@ -14,7 +16,12 @@ source venv/bin/activate
 
 pip install boto3
 
-python ./src/scan_for_sc_parent_stack.py
+TS=$(date +%Y%m%d-%H%M%S)
+LOG_FILE="${SAVE_DIR}/logs/${TS}-scan_for_sc_parent_stack.log"
+
+echo "LOG_FILE: ${LOG_FILE}"
+
+python ./src/scan_for_sc_parent_stack.py | tee "${LOG_FILE}"
 
 deactivate
 
